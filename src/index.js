cd4a7cb24bb92b48dc98bdc1843e6d719fe43785
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-05-31 12:28:05 +0200

    Add new Url, and update index.js.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import './style.css';
 
-const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`
+const url = `https://www.themealdb.com/api/json/v1/1/categories.php`
 
 
 const getFoods = async () => {
     const res = await fetch(url)
     const foods = await res.json()
-    foods.meals.forEach((food) => {
+    foods.categories.forEach((food) => {
       let temp = document.createElement('template')
       temp.innerHTML = `
       <div class="card">
-      <img src="${food.strMealThumb}"/>
+      <img src="${food.strCategoryThumb}"/>
 
       <div class="meal-description">
-      <h3>${food.strMeal}</h3>
+      <h3>${food.strCategory}</h3>
       
       <div class="likes"><i class="fas fa-heart"></i><p>5likes</p></div>
   </div>
