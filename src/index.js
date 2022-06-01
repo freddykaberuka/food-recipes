053e604e26ba01fe27336104999c16b9e2052d21
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-06-01 16:17:46 +0200

    Add dataUrl file, add backgroung color, and add close button.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 
-const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
 
 const getFoods = async () => {
   const res = await fetch(url);
---
+++ src/modules/dataUrl.js
@@ -0,0 +1,1 @@
+const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
