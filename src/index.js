c5382e3149d9307b629dcd877ac26bd90de8c09b
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-06-01 16:21:30 +0200

    Add import and export Url.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
+import url from './modules/dataUrl.js';
+
 
 
 const getFoods = async () => {
--- src/modules/dataUrl.js
+++ src/modules/dataUrl.js
@@ -1,1 +1,3 @@
 const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
+
+export default url;
