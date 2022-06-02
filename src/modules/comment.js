a28c661ecb8e8e8c297fccb8f6ff3c99cdb3b94d
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-06-02 14:54:01 +0200

    Update comments.js.

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,39 +1,3 @@
-// const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/tJRup0Me8cP9OobHqxUY/likes/';
-// const sendLikes = async (id) => {
-//   const res = fetch(url, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({
-//       item_id: id,
-//     }),
-//   });
-// };
-
-
-
-// const getLikes = async (id) => {
-//   let likes;
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   data.includes((like) => {
-//     // console.log(like);
-//     if (id == like.item_id) {
-//       likes = like.likes;
-//     } else {
-//       return 0;
-//     }
-//   });
-//   console.log(likes);
-//   // return likes
-// };
-
-// export {
-//   sendLikes,
-//   getLikes,
-// };
-
 import CountComments from "./CountComments";
 class Comments {
   FetchComments = (itemId) => {
