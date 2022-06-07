70bd61463506fe3d903eb953f5e2a0d7e9caaf72
Freddy KABERUKA <freddykaberuka2009@gmail.com>
2022-06-07 15:21:12 -0700

    fix linters error

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,9 @@
-const g=(e)=> {
-  return document.getElementById(e);
-}
+const g = (e) => document.getElementById(e);
 
-const t=(i)=> {
+const t = (i) => {
   const template = document.createElement('template');
   template.innerHTML = i;
   return template.content.firstChild;
-}
+};
 
 export { g, t };
